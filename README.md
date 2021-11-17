17471baa2ca4f2107cb5ba181b512f89ea0c4a95
Posy Au-Yeung <posyay@hotmail.com>
2021-11-16 20:47:55 -0800

    Updated README and added wait methods

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # telus_automation_qa
- 
+
+Posy Au-Yeung's Telus Automation QA Interview Project
+
+Date: November 16, 2021
+
+The purpose of this project is to create a test case with Selenium (Java) for the Telus Internet user.
+
+Trade-Offs and Decisions:
+1. Java as main programming language
+   - Most proficient with using Java as a programming language
+   - Language that is most used in current job
+2. Selenium and JUnit5 as testing frameworks
+   - Familiar with Selenium by previously working on a small project
+   - JUnit5 main testing framework for current job
+3. Should have added more verification for each step
+   - Was not familiar with verification processes/features with Selenium
+   - Would have spent more time learning about Selenium
--- src/test/java/TelusUserLoginTest.java
+++ src/test/java/TelusUserLoginTest.java
@@ -1,25 +1,24 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-
-import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class TelusUserLoginTest {
     private static final String WEBDRIVER_PATH = "C:\\Users\\posya\\OneDrive\\Documents\\GitHub\\telus_automation_qa\\libs\\chromedriver_win32\\chromedriver.exe";
-    private WebDriver driver;
+    public WebDriver driver;
 
     @BeforeEach
     public void beforeEachTest(){
         System.setProperty("webdriver.chrome.driver", WEBDRIVER_PATH);
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.get("https://www.telus.com/my-account");
     }
 
@@ -31,45 +30,42 @@ public class TelusUserLoginTest {
     @Test
     public void shouldLoginAndUpgrade(){
         try {
+            //Username and password input for login
             WebElement userName = driver.findElement(By.id("idtoken1"));
             userName.sendKeys("tdvancouver.devicelab1@telusinternal.com");
-
             WebElement password = driver.findElement(By.id("idtoken2"));
             password.sendKeys("u7xPU%49");
-
             WebElement loginButton = driver.findElement(By.id("login-btn"));
             loginButton.click();
 
-            Thread.sleep(15_000);
-            //new WebDriverWait(driver,60).until(
-            //        webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
+            //Navigate to Internet webpage
             String url = "/my-telus/internet?linktype=topNavLnkInternet";
+            WebDriverWait wait = new WebDriverWait(driver, 10);
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href='" + url + "']")));
             WebElement manageInternet = driver.findElement(By.xpath("//a[@href='" + url + "']"));
             manageInternet.click();
 
-            Thread.sleep(5_000);
+            //Navigate to change Internet option
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Change internet plan']")));
             WebElement changeInternet = driver.findElement(By.xpath("//span[text()='Change internet plan']"));
             changeInternet.click();
 
-            Thread.sleep(20_000);
+            //Select plan, add addon, and continue
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[data-qa*='exe-irpc-card-cta-internet-150-30']")));
             WebElement pickPlan = driver.findElement(By.cssSelector("button[data-qa*='exe-irpc-card-cta-internet-150-30']"));
             pickPlan.click();
-
-
             WebElement addAddon = driver.findElement(By.cssSelector("button[data-qa*='exe-irpc-card-cta-telus-boost-wi-fi-expansion-pack-easy-payment']"));
             addAddon.click();
-                                                              
             WebElement continueButton = driver.findElement(By.cssSelector("button[data-qa*='exe-irpc-continue-btn']"));
             continueButton.click();
+
+            verifyCart();
         } catch (Exception e) {
             fail("Test case failed:  " + e);
         }
     }
+
+    private void verifyCart(){
+
+    }
 }
-
-
-/*<button data-qa="exe-irpc-card-cta-internet-150-30" type="button" class="Button__StyledButton-sa7xo3-0 jMqVeU" data-di-id="di-id-e3f0971a-dac75a97"><span class="Button__ButtonTextWrapper-sa7xo3-1 gTQbTh">Select This Plan</span></button>*/
-
-//Wait for element to load
-//https://stackoverflow.com/questions/11736027/webdriver-wait-for-element-using-java
-                                                              
